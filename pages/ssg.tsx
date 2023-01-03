e67e44de85d95a4356d2c9abfba167bd5b3c3dc4
masayakimura <masa_kuanoo@live.jp>
2023-01-03 23:29:44 +0900

    SSGページを作ってsupabasと連携

---
+++ pages/ssg.tsx
@@ -0,0 +1,54 @@
+import { log } from 'console'
+import { GetStaticProps, NextPage } from 'next'
+import { Layout } from '../components/layout'
+import { Notice, Task } from '../types/types'
+import { supabase } from '../utils/supabase'
+
+export const getStaticProps: GetStaticProps = async () => {
+  console.log('getStaticProps/ssg invoked')
+
+  const { data: tasks } = await supabase
+    .from('todos')
+    .select('*')
+    .order('created_at', { ascending: true })
+
+  const { data: notices } = await supabase
+    .from('notices')
+    .select('*')
+    .order('created_at', { ascending: true })
+
+  return { props: { tasks, notices } }
+}
+
+type StaticProps = {
+  tasks: Task[]
+  notices: Notice[]
+}
+
+const Ssg: NextPage<StaticProps> = ({ tasks, notices }) => {
+  return (
+    <Layout title="SSG">
+      <p className="mb-3 text-blue-500">SSG</p>
+      <ul className="mb-3">
+        {tasks.map((task) => {
+          return (
+            <li key={task.id}>
+              <p className="flex-lg font-extrabold">{task.title}</p>
+            </li>
+          )
+        })}
+      </ul>
+      <ul className="mb-3">
+        {notices.map((notice) => {
+          return (
+            <li key={notice.id}>
+              <p className="flex-lg font-extrabold">{notice.content}</p>
+            </li>
+          )
+        })}
+      </ul>
+    </Layout>
+  )
+}
+
+export default Ssg
